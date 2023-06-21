260172a4ec480d7d838ec326fcaf908486962458
Scott, Richard (Contractor) <Richard_Scott@comcast.com>
2023-06-21 18:16:02 -0400

    fix: removed env variables

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
   API_ENDPOINT: 'https://ways-project.herokuapp.com/api',
   TOKEN_KEY: 'blogful-client-auth-token',
-  MAP_API_KEY: 'AIzaSyABLbzSij1WHiacsFD9Lonevg1cB7X8jSs',
-  SEARCH_API_KEY: 'AIzaSyABLbzSij1WHiacsFD9Lonevg1cB7X8jSs',
+  MAP_API_KEY: process.env.REACT_APP_API_KEY || '',
+  SEARCH_API_KEY: process.env.REACT_APP_API_KEY || '',
 };
